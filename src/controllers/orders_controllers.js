4d69d014610782a6e8326cce0a19fedae0d2250f
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-05 10:07:32 -0500

    Select operation success

--- src/controllers/orders_controllers.js
+++ src/controllers/orders_controllers.js
@@ -1,9 +1,17 @@
-const controller = {}; 
+const controller = {};
 
 controller.home = (req, res) => {
-    
-    res.render('index'); 
+    //Controlador de la ruta get('/')
 
-}
+    req.getConnection((err, conn) => {
+        //Cuando obtenga la conección, se hace el query
+        conn.query('SELECT * FROM orders', (err, rows) => {
+            //Cuando obtenga las rows, las manda al index
+            res.render('index', {
+                data: rows, //Le manda como información las rows obtenidas en la query
+            });
+        });
+    });
+};
 
-module.exports = controller; 
+module.exports = controller;
