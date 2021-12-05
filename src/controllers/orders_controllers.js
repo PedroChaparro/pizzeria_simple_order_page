f790b1b22714f91c14d5f681dfbef503661ed0e3
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-05 10:32:08 -0500

    Insert operation success

--- src/controllers/orders_controllers.js
+++ src/controllers/orders_controllers.js
@@ -1,8 +1,7 @@
 const controller = {};
 
+//Controlador de la ruta get('/')
 controller.home = (req, res) => {
-    //Controlador de la ruta get('/')
-
     req.getConnection((err, conn) => {
         //Cuando obtenga la conección, se hace el query
         conn.query('SELECT * FROM orders', (err, rows) => {
@@ -11,6 +10,18 @@ controller.home = (req, res) => {
                 data: rows, //Le manda como información las rows obtenidas en la query
             });
         });
+    });
+};
+
+//Controlador de la ruta post('/add');
+controller.add = (req, res) => {
+    const order_data = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO orders SET ?', [order_data], (err, order) => {
+            //Cuando inserta el dato lo redirecciona a la página inicial
+            res.redirect('/'); 
+        }); 
     });
 };
 
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require('../controllers/orders_controllers'); 
 
 router.get('/', controller.home); 
+router.post('/add', controller.add); 
 
 module.exports = router; 
