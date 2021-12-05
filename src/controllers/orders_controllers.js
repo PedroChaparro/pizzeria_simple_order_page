e1cb350d804a53c34015a523d49125f80995af3b
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-05 11:01:19 -0500

    Change and update operation success

--- src/controllers/orders_controllers.js
+++ src/controllers/orders_controllers.js
@@ -20,9 +20,39 @@ controller.add = (req, res) => {
     req.getConnection((err, conn) => {
         conn.query('INSERT INTO orders SET ?', [order_data], (err, order) => {
             //Cuando inserta el dato lo redirecciona a la página inicial
-            res.redirect('/'); 
-        }); 
+            res.redirect('/');
+        });
     });
+};
+
+//Controlador de la ruta para obtener los datos de una ruta y cambiarlos
+controller.change = (req, res) => {
+    const { id } = req.params;
+
+    //Se hace la consulta para obtener los datos a cambiar
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM orders WHERE id = ?', id, (err, order) => {
+            res.render('change', {
+                data: order[0], //Se le pasa la posición 0 ya que la query retorna una lista
+            });
+        });
+    });
+};
+
+controller.update = (req, res) => {
+
+    const { id } = req.params; 
+    const new_data = req.body; 
+
+    //Se hace el query para actualizar los datos en la BD
+    req.getConnection((err, conn) => {
+
+        conn.query('UPDATE orders SET ? WHERE id = ?', [new_data, id], (err, row) => {
+            res.redirect('/'); //Cuando actualice lo redirecciona a la ruta raíz
+        }); 
+
+    }); 
+
 };
 
 module.exports = controller;
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
 
-const controller = require('../controllers/orders_controllers'); 
+const controller = require('../controllers/orders_controllers');
 
-router.get('/', controller.home); 
-router.post('/add', controller.add); 
+router.get('/', controller.home); //Ruta home que muestra la lista de ordenes y el formulario para agregar una nueva orden
+router.post('/add', controller.add); //Ruta add que se activa cuadno se da click en el botón de "ADD ORDER"
 
-module.exports = router; 
+//Editar y guardar cambios en la orden
+router.get('/change/:id', controller.change); //Ruta para modificar los datos de una orden
+router.post('/update/:id', controller.update);  //Ruta para guardar los cambios aplicados a una orden
+
+module.exports = router;
